9c702d7954c10e88f9fb5679629a95f065784bef
ngrhanto <163571036+ngrhanto@users.noreply.github.com>
2025-07-12 12:53:16 +0700

    Add files via upload

--- src/utils/site.js
+++ src/utils/site.js
@@ -1,9 +1,9 @@
 export const url = 'https://bokepperkosa.pages.dev';
 export const nama = 'Bokep Perkosa';
 export const desk = 'Video Bokep Indo Jepang Terbaru Simontok Bocil Ngentot Jilbab Smp Viral Sma Tante';
-export const terbit = '2024-01-09T00:01:03Z';
+export const terbit = '2024-02-17T00:01:03Z';
 
 export const google = 'mT1gLBKJQNXx44o6uCm3ReWJfU2SHnVAryhdnvY2eiY';
 export const yandex = '893254ba4838fcec';
 
-export const index = '35635a02-cd6a-48d6-9b23-2b056e141bde';
+export const index = '4da8190e-bc1a-42cf-bfea-73b8bb8b2f95';
