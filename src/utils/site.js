cac2a6da63d909692d4313bee4f78453d1ed4391
ngrhanto <163571036+ngrhanto@users.noreply.github.com>
2025-07-12 12:43:38 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -1,9 +1,9 @@
-export const url = 'https://bokepmamah.pages.dev';
-export const nama = 'Bokep Mamah';
+export const url = 'https://bokepperkosa.pages.dev';
+export const nama = 'Bokep Perkosa';
 export const desk = 'Video Bokep Indo Jepang Terbaru Simontok Bocil Ngentot Jilbab Smp Viral Sma Tante';
 export const terbit = '2024-01-09T00:01:03Z';
 
 export const google = 'mT1gLBKJQNXx44o6uCm3ReWJfU2SHnVAryhdnvY2eiY';
-export const yandex = 'a502f77311c61675';
+export const yandex = '893254ba4838fcec';
 
 export const index = '35635a02-cd6a-48d6-9b23-2b056e141bde';
